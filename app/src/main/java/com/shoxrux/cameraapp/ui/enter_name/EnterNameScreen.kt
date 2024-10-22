e9138e94b6c0681aeb0fec66b67e858a65863835
Shoxrux061 <shoxruxurmanov2@gmail.com>
2024-10-22 21:56:42 +0500

    commit

---
+++ app/src/main/java/com/shoxrux/cameraapp/ui/enter_name/EnterNameScreen.kt
@@ -0,0 +1,18 @@
+package com.shoxrux.cameraapp.ui.enter_name
+
+import android.os.Bundle
+import android.view.View
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.shoxrux.cameraapp.R
+import com.shoxrux.cameraapp.databinding.ScreenEnterNameBinding
+import com.shoxrux.cameraapp.ui.base.BaseFragment
+
+class EnterNameScreen : BaseFragment(R.layout.screen_enter_name) {
+
+    private val binding by viewBinding(ScreenEnterNameBinding::bind)
+
+    override fun onCreate(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+}
